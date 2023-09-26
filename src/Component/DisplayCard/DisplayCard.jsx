f269ba95d38d98acf195dcc3cca6e296975b1652
Towhidul123 <excaliburfsh@gmail.com>
2023-09-26 18:56:43 +0600

    created donation listed page

--- src/Component/DisplayCard/DisplayCard.jsx
+++ src/Component/DisplayCard/DisplayCard.jsx
@@ -4,19 +4,79 @@ const DisplayCard = ({ category }) => {
 
     const { id, img, title, categories, category_bg, card_bg, text_button_bg, description, price } = category || {}
 
-    console.log({ title })
-    return (
-        <div className="relative h-[700px] w-full">
-            <figure className="h-full w-full rounded-xl object-cover object-center">
-                <img src={img} alt="" />
-            </figure>
+    const buttonBgStyle ={
+        backgroundColor: category_bg,
+    };
 
-            <div className="absolute  bottom-0 w-[calc(100%)]    bg-black/50 py-4 px-6   saturate-200 ">
-                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Donate ${price}</button>
+    const handleDonate = () =>{
+      //  console.log(category);
+
+      const addedItems =[];
+
+      const items = JSON.parse(localStorage.getItem('test'))
+     //   console.log(items)
+
+        if(!items){
+            addedItems.push(category)
+            localStorage.setItem('test',JSON.stringify(addedItems))
+            alert('products added')
+        }
+
+        else{
+
+            const ifExist = items.find(category => category.id == id)
+            
+            if(!ifExist){
+                addedItems.push(...items,category)
+                localStorage.setItem('test',JSON.stringify(addedItems))
+                alert('products added')
+            }
+
+           
+            else{
+                alert('already exist')
+            }
+        }
+       
+    }
+
+ 
+   
+   
+    
+
+    return (
+
+       
+        <div >
+            <div className="relative ">
+
+
+
+                <figure className="">
+                    <img className="h-[700px] w-full object-cover object-center" src={img} alt="" />
+                </figure>
+
+                <div className="absolute  bottom-0 w-[calc(100%)]    bg-black/50 py-4 px-6   saturate-200 ">
+                    <button onClick={handleDonate} style={buttonBgStyle} className={`btn  btn-xs  sm:btn-sm md:btn-md lg:btn-lg text-white`}>Donate ${price}</button>
+                </div>
+
             </div>
 
+            <div>
+                <h2 className="text-[40px] font-bold text-[#0B0B0B]">{title}</h2>
+                <p className="text-base text-[#545454]">{description}</p>
+
+                
+            </div>
+
+                
 
         </div>
+
+
+
+
     );
 };
 
--- src/Component/Donation/Donation.jsx
+++ src/Component/Donation/Donation.jsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from "react";
+import CategoryCard from "../CategoryCard/CategoryCard";
 
 
 const Donation = () => {
-    return (
-        <div>
-            This is donation
-        </div>
-    );
+
+    const [donation,setDonation] = useState([])
+
+    const [noFound,setNoFound] = useState (false)
+
+    useEffect (() =>{
+        const items = JSON.parse(localStorage.getItem('test'));
+        
+        if(items){
+            setDonation(items)
+
+        }
+
+        else{
+            
+            setNoFound("No Data Found")
+        }
+        
+
+
+    },[])
+
+    console.log(donation)
+
+    return  (
+            <div>
+                {
+                    noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> : <div>
+
+                    <div className="grid grid-cols-2 gap-5">
+                        {donation.map(category=><CategoryCard key={category.id} category={category}></CategoryCard>)}
+                    </div>
+
+
+                    <button  className="px-5 bg-green-200 block mx-auto">See More</button>
+
+
+
+                    </div>
+                }
+            </div>
+        );
 };
 
 export default Donation;
