97ef359a1291ebd86cbedb1dbe493cea6857684c
Towhidul123 <excaliburfsh@gmail.com>
2023-09-27 16:31:37 +0600

    finished making the pie chart

--- src/Component/Statistics/Statistics.jsx
+++ src/Component/Statistics/Statistics.jsx
@@ -1,9 +1,58 @@
-
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer,  Legend } from 'recharts';
 
 const Statistics = () => {
+    const COLORS = ['#FF444A', '#00C49F'];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+    const [stats, setStats] = useState(12); 
+
+    useEffect(() => {
+        const items = JSON.parse(localStorage.getItem('test'));
+        if (items && items.length > 0) {
+            setStats(items.length); 
+        }
+    }, []);
+
+    const pieData = [
+        { name: 'Total Donation', value: 12 - stats } ,
+        { name: 'Your Donation', value: stats }
+        
+    ];
+
     return (
-        <div>
-            This is statistics
+        <div className=''>
+            <ResponsiveContainer width="100%" height={600}>
+                <PieChart>
+                    <Pie
+                        data={pieData}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={150}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {pieData.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Legend align="center"  verticalAlign="bottom" layout="horizontal" iconType="circle" />
+                </PieChart>
+            </ResponsiveContainer>
         </div>
     );
 };
